aa399b11c3cb6b4b0fa771ad05afabf4c8b376b8
EMGEE2605 <mosesgeorge2605@gmail.com>
2023-01-24 09:40:24 +0100

    new changes

--- src/Home_Page.js
+++ src/Home_Page.js
@@ -1,7 +1,22 @@
-// example of a simple form validation
-const form = document.querySelector("form");
-form.addEventListener("submit", validateForm);
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function validateForm(event) {
-  event.preventDefault();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
+
